07b4e56b1152ffdf601a9d8a13c9e13b5f9625b9
kimura miyuki <hcp.miyuki@gmail.com>
2020-05-22 16:34:30 +0900

    [init]__init__.pyのversion

--- tweetl/__init__.py
+++ tweetl/__init__.py
@@ -2,7 +2,7 @@ from tweetl.get_tweets import GetTweet
 from tweetl.cleansing_tweets import CleansingTweets
 
 __copyright__    = 'Copyright (C) 2020 deepblue'
-__version__      = '0.0.2'
+__version__      = '0.0.1'
 __license__      = 'MIT'
 __author__       = 'deepblue'
 __author_email__ = 'main@deepbluets.page'
